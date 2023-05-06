fc3d7ecda4f7264618bb0ffa951a608f2309c651
tejuingale <teju9075431155@gmail.com>
2023-05-06 06:59:18 +0530

    first java programm
    first java programm

---
+++ hello.js
@@ -0,0 +1,17 @@
+
+
+
+
+console.log("Tejashvini Ingale..");
+console.log("Ganesh Chavan..");
+var age;  //variable Declaration
+age = "22"; //variable Initilization
+console.log(age);
+
+var holder;
+console.log(holder); //undefined
+holder = "My Spec"; //variable Initilization
+console.log(holder);
+
+holder = "Sachin Spec"; //variable update
+console.log(holder);
---
+++ variable__assign.js
@@ -0,0 +1,23 @@
+
+
+
+
+var myFullName ;
+console.log(myFullName);  //undefined
+myFullName = "Tejashvini Ingale";
+console.log(myFullName);
+myFullName = "Tejashvini Naganath Ingale";
+console.log(myFullName);
+var myLove = "I love only JS";
+console.log(myLove);
+
+var form ="Tejashvini Ingale  27  A/P.Suste,Tal-Pandharpur,Dist-Solapur  413304  Female  yes";
+console.log(form);
+
+
+
+
+
+
+
+
