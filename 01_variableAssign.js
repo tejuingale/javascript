8e2dba4a1caa38420a2be8e477eece060fadf43b
tejuingale <teju9075431155@gmail.com>
2023-05-07 13:12:26 +0530

    primitive data type
     primitive data type

---
+++ 01_variableAssign.js
@@ -0,0 +1,31 @@
+
+
+
+
+var myFullName ;
+console.log(myFullName);  //undefined
+myFullName = "Tejashvini Ingale";
+console.log(myFullName);
+myFullName = "Tejashvini Naganath Ingale";
+console.log(myFullName);
+var myLove = "I love only JS";
+console.log(myLove);
+var firstName = "Tejashvini";
+var lastName = "Ingale";
+var age = "27";
+var address = "A/P.Suste,Tal-Pandharpur,Dist-Solapur";
+var pinCode = "413304";
+var gender = "Female";
+var isMarride = "yes";
+
+
+
+console.log(firstName,lastName,age,address,pinCode,gender,isMarride);
+
+
+
+
+
+
+
+
