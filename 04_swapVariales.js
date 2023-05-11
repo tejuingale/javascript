efc086bf36b4efd5abb87befe0a9a6096c4eb694
tejuingale <teju9075431155@gmail.com>
2023-05-11 15:35:43 +0530

    function in javascript
    function in javascript

---
+++ 04_swapVariales.js
@@ -0,0 +1,22 @@
+
+console.log("--------------------swap two variable------------");
+console.log("---------------------------------------------------");
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("Beforeswap---" , "Name1:",sweety, "    Name2: ",cutie);
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("Afterswap---" , "Name1:",sweety, "   Name2: ",cutie);
+
+console.log("--------------------swap three variable------------");
+console.log("---------------------------------------------------");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("Beforeswap---" , "Num1:",num1, "   Num2:",num2, "   Num3:",num3);
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("Afterswap---" , "Num1:",num1, "   Num2:",num2, "   Num3:",num3);
